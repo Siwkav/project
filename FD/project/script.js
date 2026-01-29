e5c43ee503044ba7c4dcbff4b098d5346d2c4824
Wftap <bnurik321@gmail.com>
2026-01-29 22:29:47 +0500

    Delete FD/project directory

--- FD/project/script.js
+++
@@ -1,8 +0,0 @@
-function openModal(img, name) {
-  document.getElementById('modal-img').src = img;
-  document.getElementById('modal-name').textContent = name;
-  document.getElementById('modal').style.display = 'flex';
-}
-function closeModal() {
-  document.getElementById('modal').style.display = 'none';
-}
